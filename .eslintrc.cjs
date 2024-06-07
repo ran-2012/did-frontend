d0567d66af66b161f9ec709b0f60f69bbaaae9fe
ran <ranto2012@163.com>
2024-06-07 22:54:01 +0800

    CHORE: Setup project.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,19 @@
 module.exports = {
-    root: false,
+    root: true,
     env: {browser: true, es2020: true},
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
         'plugin:import/recommended',
         'plugin:import/typescript',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
     rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
-        ],
         'no-unused-vars': [
             'warn',
             {"args": "none"}
         ],
         'import/order': 'warn',
-        'import/no-unresolved': 'off',
     },
 }
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,13 +1,21 @@
+import * as path from 'path'
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
-    process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')}
+    const envFileDirectory = path.relative(process.cwd(), '../..');
+    process.env = {...process.env, ...loadEnv(mode, envFileDirectory, '')}
+    console.log("Loading env from ", envFileDirectory)
+    for (const key in process.env) {
+        if (key.startsWith('VITE')) {
+            console.log(key, process.env[key])
+        }
+    }
 
     return {
         plugins: [react()],
-        server:{
+        server: {
             watch: {
                 usePolling: true
             }
